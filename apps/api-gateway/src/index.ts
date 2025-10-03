53a7617ecd07787ef912a71ae81197e00f3d3a54
Risshi25 <risshirajsen@gmail.com>
2025-10-04 01:38:18 +0530

    add/basic route definition

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -1,18 +1,23 @@
 import app from './logger.js';
 import { logger } from './logger.js';
 import redis from '@fastify/redis';
+import userRoutes from './routes/user.routes.js'
 
+// register a redis client
 app.register(redis, {
   host: process.env.REDIS_HOST || 'redis',
   port: Number(process.env.REDIS_PORT) || 6379,
   closeClient: true,
 });
 
+// register routes
+app.register(userRoutes,{prefix:'/api'})
+
 app.get('/', async (req, res) => {
   return { success: '1000' };
 });
 
-app.listen({ port: 3000 }, async (err, address) => {
+app.listen({ port: 3000 , host:'0.0.0.0'}, async (err, address) => {
   if (err) throw err;
 
   try {
---
+++ apps/api-gateway/src/routes/user.routes.ts
@@ -0,0 +1,17 @@
+import type { FastifyInstance, RouteGenericInterface } from 'fastify';
+
+interface RegisterRoute extends RouteGenericInterface {
+    Querystring: {
+        username: string;
+    };
+}
+
+export default async function (app: FastifyInstance, ops: any) {
+    app.get<RegisterRoute>('/register', {}, async (request, reply) => {
+        const username = request.query.username; 
+        reply.code(201);
+        return {
+            "hello": username // Return the actual username for verification
+        };
+    });
+}
